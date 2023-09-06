28a94867fe8bedf11e9f583c3f785f5a10133cc6
Ram Singh <ram.singh@talentmonk.com>
2023-09-06 22:58:27 +0530

    integrated with tailwind css

--- aims/src/App.js
+++ aims/src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>this is ram/header branch</h1>
+    <div className="App ">
+      <h1 className='font-bold'>this is ram/header branch</h1>
       <h1>AIMS Portal ram change kiya</h1>
     </div>
   );
