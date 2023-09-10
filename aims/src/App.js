5af387014ed4bf9bc9d3215224e3b05f064f8f06
Ram Singh <ram.singh@talentmonk.com>
2023-09-10 14:26:42 +0530

    improved folder structure

--- aims/src/App.js
+++ aims/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router,Routes,Route} from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar';
-import Header from './components/Header/Header';
+import Navbar from './layouts/navbar/Navbar';
+import Header from './layouts/header/Header';
 import About from './Pages/About';
 import PersonalDetails from './Pages/PersonalDetails';
 import Grade from './Pages/Grade';
