152c74910f26aab8d05d5249e106444fe4a9e7a1
prithvi5481 <prithvisingh91827@gmail.com>
2023-10-27 00:44:44 +0530

    personal details page designed

--- aims/src/Pages/PersonalDetails.jsx
+++ aims/src/Pages/PersonalDetails.jsx
@@ -1,13 +1,55 @@
-import React from 'react'
-import Auth from './Auth/Auth';
+import React from 'react';
 
 const PersonalDetails = () => {
-  return (
-    <div className='pl-4 pt-4'>
-      PersonalDetails component
-    </div>
-    
-  )
-}
+  // Sample user data (you can load this from your database)
+  const userData = {
+    name: 'John Doe',
+    rollNo: '20232',
+    dateOfBirth: '20-02-2000',
+    email: 'johndoe@example.com',
+    phone: '+91 6542024510',
+    address: '123 Main Street, City, Country',
+    semester: '7',
+    batch:"2020-24"
+    // Add more personal details as needed
+  };
 
-export default PersonalDetails
+  return (
+    <div className='p-4'>
+      <div className='mx-32'>
+        <div className='border rounded-md h-24 shadow-md'>
+            <div className='flex justify-center py-7'>
+              <p className='font-bold text-2xl'>Personal Details</p>
+            </div>
+        </div>
+      </div>
+      <div className='mt-14'>
+        <div className='mx-32 bg-gray-200 border rounded-md shadow-lg p-4'> 
+          <div className='flex flex-col gap-2'>
+              <div className='py-2'>
+                <h2 className='font-bold text-2xl'><strong>Welcome, {userData.name}</strong></h2>
+              </div>
+              <div>
+                <div className='grid grid-cols-2 gap-4'>
+                    <div>
+                      <p><strong>Roll No. </strong>{userData.rollNo}</p>
+                      <p><strong>Semester: </strong>{userData.semester}</p>
+                      <p><strong>Email: </strong>{userData.email}</p>
+                    
+                      <p><strong>Batch: </strong>{userData.batch}</p>
+                    </div>
+                    <div>
+                      <p><strong>Date of Birth: </strong>{userData.dateOfBirth}</p>
+                      <p><strong>Contact: </strong>{userData.phone}</p>
+                      <p><strong>Address: </strong>{userData.address}</p>
+                    </div>
+                </div>
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PersonalDetails;
