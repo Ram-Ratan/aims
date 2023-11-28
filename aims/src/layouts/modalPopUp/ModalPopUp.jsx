0153167df46e871a4b0230e57180213b114624e8
Ram Singh <ram.singh@talentmonk.com>
2023-11-28 16:48:33 +0530

    modal popup added

---
+++ aims/src/layouts/modalPopUp/ModalPopUp.jsx
@@ -0,0 +1,44 @@
+import { XMarkIcon } from "@heroicons/react/24/solid";
+import React from "react";
+
+const ModalPopup = ({
+  onClose,
+  onCallBack,
+  modalType,
+  title,
+  body,
+  bodyText,
+  height,
+  width,
+}) => {
+  return (
+    <div className="fixed z-[53] inset-0 bg-backdrop ">
+      <div className="flex items-center justify-center min-h-screen px-4 w-full">
+        <div
+          className={`bg-white rounded-lg shadow-md relative   pb-4 ${
+            width ? width : "w-[600px] max-w-[600px]"
+          }`}
+        >
+          <div className="flex justify-between p-4 font-semibold">
+            <p className="items-center">{title}</p>
+            <button
+              className="flex justify-end items-end top-0 right-0 p-2 text-gray-500 hover:text-gray-700"
+              onClick={onClose}
+            >
+              <XMarkIcon className="w-5 h-5" />
+            </button>
+          </div>
+          <div
+            className={`${
+              height ? `max-h-[${height}]` : "max-h-[400px]"
+            } overflow-y-auto custom-scrollbar`}
+          >
+            {body}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ModalPopup;
